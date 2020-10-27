0475d8c51ccd1990bf8dc3c2b490944372a7f8ce
seabelis <seabelis74@gmail.com>
2020-10-27 20:42:22 +0100

    Added submitting, setSubmitting.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [submitting, setSubmitting] = useState(false)
   const handleSubmit = event => {
-    event.preventDefault();
-    alert('URL added for indexing.')
+    event.preventDefault()
+    setSubmitting(true)
+
+    setTimeout(() => {
+      setSubmitting(false)
+    }, 3000)
   }
+
   return (
     <div className="wrapper">
       <h1>URL Manager</h1>
+      {submitting && 
+        <div>Submitting Form...</div>
+      }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
