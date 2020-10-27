e9c24efea37b893f0e3ae9648fbbee71d9bfc118
seabelis <seabelis74@gmail.com>
2020-10-27 20:30:16 +0100

    Modify CSS. Add handleSubmit.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert('URL added for indexing.')
+  }
   return (
     <div className="wrapper">
       <h1>URL Manager</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
             <p>URL</p>
             <input name="name" />
           </label>
         </fieldset>
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
